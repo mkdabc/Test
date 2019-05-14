d9a58e1e17be3d26ef76d7bb40dd16b18de7c388
mao <458351424@qq.com>
2019-05-14 19:01:41 +0800

    第二版本

--- new.py
+++ new.py
@@ -1,10 +1,25 @@
-def new():
-    print('1123')
+# def new():
+#     print('1123')
+#
+# def login():
+#     print('2123')
+#
+# def go():
+#     pass
+#
+# list = [1,2,3,4]
 
-def login():
-    print('2123')
+import random
 
-def go():
-    pass
+def v_code():
 
-list = [1,2,3,4]
+    code = ''
+    for i in range(5):
+        num=random.randint(0,9)
+        alf=chr(random.randint(65,90))
+        add=random.choice([num,alf])
+        code="".join([code,str(add)])
+        print(alf)
+    return code
+
+print(v_code())
